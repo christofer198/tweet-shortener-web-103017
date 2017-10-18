bc7145e54361da53b1351c1c05e782ae8b869967
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-18 18:42:17 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def dictionary
+  dictionary = {"hello"=>'hi', "to"=>'2', "two"=>'2', "too"=>'2', "for"=>'4', "four"=>'4', 'be'=>'b', 'you'=>'u', "at"=>"@", "and"=>"&"}
+end
+
+def word_substituter(tweet)
+  arr = tweet.split(" ")
+    arr.each_with_index do |word,index|
+        if dictionary[word] != nil
+            arr[index] = dictionary[word]
+        end
+    end
+    arr.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |each_tweet|
+    puts word_substituter(each_tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+    if tweet.length > 140
+        word_substituter(tweet)
+    else
+        tweet
+    end
+end
+
+def shortened_tweet_truncator(tweet)
+    sub = word_substituter(tweet)
+    if sub > 140
+        puts "#{sub[1...137]}..."
+    else
+        sub
+    end
+end
